31547d1bb9c27302dcf7b32b741ed1b72184ebbf
Nicolo <lukakuchaidze@gmail.com>
2023-03-06 22:27:32 +0400

    Start

---
+++ scripts.js
@@ -0,0 +1,200 @@
+
+// Slider
+
+var ListLi = document.querySelectorAll('#ul li');
+var raodenoba = ListLi.length;
+localStorage.slittyui = 0;
+var toNumber = null;
+var j = 0;
+
+function forward() {
+    toNumber = parseInt(localStorage.slittyui);
+    var logo1 = document.querySelectorAll(".sliderimage");
+    var ListLi = document.querySelectorAll('#ul li');
+
+    if (toNumber < raodenoba - 1) {
+        toNumber += 1;
+        for (var j = 0; j < ListLi.length; j++) {
+            ListLi[j].style.backgroundColor = "#fff";
+        }
+        localStorage.slittyui = toNumber;
+        ListLi[toNumber].style.backgroundColor = "#42c3f5";
+        localStorage.slittyui = toNumber;
+
+
+
+        for (var j = 0; j < ListLi.length; j++) {
+            logo1[j].style.opacity = "0";
+        }
+        logo1[toNumber].style.opacity = "1";
+    }
+    else {
+        toNumber = 0;
+        for (var j = 0; j < ListLi.length; j++) {
+            ListLi[j].style.backgroundColor = "#fff";
+        }
+        localStorage.slittyui = toNumber;
+        ListLi[toNumber].style.backgroundColor = "#42c3f5";
+        localStorage.slittyui = toNumber;
+
+
+
+        for (var j = 0; j < ListLi.length; j++) {
+            logo1[j].style.opacity = "0";
+        }
+        logo1[toNumber].style.opacity = "1";
+    }
+
+
+}
+function back() {
+    toNumber = parseInt(localStorage.slittyui);
+    var logo1 = document.querySelectorAll(".sliderimage");
+    var ListLi = document.querySelectorAll('#ul li');
+
+    if (toNumber != 0) {
+        toNumber -= 1;
+        for (var j = 0; j < ListLi.length; j++) {
+            ListLi[j].style.backgroundColor = "#fff";
+        }
+        localStorage.slittyui = toNumber;
+        ListLi[toNumber].style.backgroundColor = "#42c3f5";
+        localStorage.slittyui = toNumber;
+
+
+
+        for (var j = 0; j < ListLi.length; j++) {
+            logo1[j].style.opacity = "0";
+        }
+        logo1[toNumber].style.opacity = "1";
+    }
+    else {
+        toNumber = ListLi.length - 1;
+        for (var j = 0; j < ListLi.length; j++) {
+            ListLi[j].style.backgroundColor = "#fff";
+        }
+        localStorage.slittyui = toNumber;
+        ListLi[toNumber].style.backgroundColor = "#42c3f5";
+        localStorage.slittyui = toNumber;
+
+
+
+        for (var j = 0; j < ListLi.length; j++) {
+            logo1[j].style.opacity = "0";
+        }
+        logo1[toNumber].style.opacity = "1";
+    }
+
+
+}
+
+
+
+
+var gffjjh = setInterval(function autoChange() {
+
+    var logo1 = document.querySelectorAll(".sliderimage");
+
+
+
+    toNumber = parseInt(localStorage.slittyui);
+    for (var j = 0; j < ListLi.length; j++) {
+        ListLi[j].style.backgroundColor = "#fff";
+    }
+    if (toNumber < raodenoba - 1) {
+        toNumber += 1;
+        localStorage.slittyui = toNumber;
+        ListLi[toNumber].style.backgroundColor = "#42c3f5";
+        localStorage.slittyui = toNumber;
+
+    }
+    else {
+        toNumber = 0;
+        localStorage.slittyui = toNumber;
+        ListLi[toNumber].style.backgroundColor = "#42c3f5";
+    }
+
+    for (var j = 0; j < ListLi.length; j++) {
+
+
+
+        logo1[j].style.opacity = "0";
+
+
+
+    }
+
+
+
+    logo1[toNumber].style.opacity = "1";
+
+
+
+}, 5000);
+
+
+function myFunction() {
+    gffjjh = setInterval(function autoChange() {
+
+
+        var logo1 = document.querySelectorAll(".sliderimage");
+
+
+        toNumber = parseInt(localStorage.slittyui);
+        for (var j = 0; j < ListLi.length; j++) {
+            ListLi[j].style.backgroundColor = "#fff";
+        }
+        if (toNumber < raodenoba - 1) {
+            toNumber += 1;
+            localStorage.slittyui = toNumber;
+            ListLi[toNumber].style.backgroundColor = "#42c3f5";
+            localStorage.slittyui = toNumber;
+        }
+        else {
+            toNumber = 0;
+            localStorage.slittyui = toNumber;
+            ListLi[toNumber].style.backgroundColor = "#42c3f5";
+        }
+        for (var j = 0; j < ListLi.length; j++) {
+
+
+
+            logo1[j].style.opacity = "0";
+
+
+        }
+
+
+        logo1[toNumber].style.opacity = "1";
+
+    }, 5000);
+
+
+}
+
+function myStopFunction() {
+    clearInterval(gffjjh);
+
+}
+
+function changeSlider(el) {
+    var elItem = parseInt(el.dataset.items);
+
+
+    var logo1 = document.querySelectorAll(".sliderimage");
+
+
+    for (var j = 0; j < ListLi.length; j++) {
+        ListLi[j].style.backgroundColor = "#fff";
+    }
+    el.style.backgroundColor = "#42c3f5";
+    localStorage.slittyui = elItem;
+
+    for (var j = 0; j < ListLi.length; j++) {
+        logo1[j].style.opacity = "0";
+    }
+
+
+    logo1[elItem].style.opacity = "1";
+
+}
---
+++ ss
@@ -0,0 +1,30 @@
+<section class="hero-section">
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    
+    <!-- Indicators/dots -->
+    <ol class="carousel-indicators">
+      <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+      <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>     
+    </ol>
+  
+    <!-- Slides -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./img/banner-1.png" alt="Slide 1">
+      </div>
+      <div class="carousel-item">
+        <img src="./img/banner-2.png" alt="Slide 2">
+      </div>
+    </div>
+  
+    <!-- Navigation buttons -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+ </section>
