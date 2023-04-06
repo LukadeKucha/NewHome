01b0309220475e76319868a914c7cef3247b4cc5
Nicolo <lukakuchaidze@gmail.com>
2023-04-06 23:07:52 +0400

    Rooms Desc.

--- floor.js
+++ floor.js
@@ -12,14 +12,17 @@ $(document).ready(function () {
 	document.addEventListener("mousemove", function (a) {
 		var x = a.clientX;
 		var y = a.clientY;
-		cursor_room.style.left = x - 230 + 'px'
-		cursor_room.style.top = y - 240 + 'px'
+		cursor_room.style.left = x - 250 + 'px'
+		cursor_room.style.top = y - 260 + 'px'
 	});
 });
 
 function Floor_namber(el) {
+
 	var room = el.dataset.room;
 	var kv = el.dataset.kv;
+	var kv2 = el.dataset.kv2;
+	var kv3 = el.dataset.kv3;
 	var kvs = el.dataset.kvs;
 
 	if (kvs == '2') {
@@ -42,13 +45,17 @@ function Floor_namber(el) {
 		document.querySelector(".Room_info_tex").innerHTML = 'სასტუმრო';
 		document.querySelector(".Room_info_tex").style.display = 'block';
 		document.querySelector(".room_info").innerHTML = room;
-		document.querySelector(".room_kv").innerHTML = kv;
+		document.querySelector(".room_kv").innerHTML =  kv;
+		document.querySelector(".room_kv2").innerHTML = 'საზაფხულე: ' + kv2;
+		document.querySelector(".room_kv3").innerHTML ='საცხოვრებელი: ' + kv3;
 	}
-	else {
-		document.querySelector(".Room_info").style.height = '80px';
+	else if(kvs==='1') {
+		document.querySelector(".Room_info").style.height = '120px';
 		document.querySelector(".room_info").innerHTML = room;
-		document.querySelector(".room_kv").innerHTML = kv;
 		document.querySelector(".Room_info_tex").style.display = 'none';
+		document.querySelector(".room_kv").innerHTML =  kv;
+		document.querySelector(".room_kv2").innerHTML = 'საზაფხულე: ' + kv2;
+		document.querySelector(".room_kv3").innerHTML ='საცხოვრებელი: ' + kv3;
 	}
 
 }
