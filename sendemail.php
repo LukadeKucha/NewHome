48c232b5f9681f85f58b361239d4728e11e9ea71
Nicolo <lukakuchaidze@gmail.com>
2023-05-24 15:49:17 +0400

    .

--- sendemail.php
+++ sendemail.php
@@ -1,30 +1,25 @@
-
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $subject = $_POST['sub'];
+  $message = $_POST['message'];
 
-// Check for empty fields
-if(empty($_POST['name']) ||
-   empty($_POST['email']) ||
-   empty($_POST['phone']) ||
-   empty($_POST['sub']) ||
-   empty($_POST['message']) ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
+  // Construct the email headers and body
+  $to = 'lukakuchaidze@gmail.com'; // Replace with the email account you created in Vesta
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+  $body = "Name: $name\nEmail: $email\nPhone: $phone\nSubject: $subject\n\n$message";
 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$sub = $_POST['sub'];
-$message = $_POST['message'];
+  // Send the email
+  $result = mail($to, $subject, $body, $headers);
 
-// Create the email and send the message
-$to = 'lukakuchaidze@gmail.com'; // Add your email address here
-$email_subject = "Website Contact Form: $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nSubject: $sub\n\nMessage:\n$message";
-$headers = "From: noreply@your-domain.com\n";
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+  if ($result) {
+    echo 'Email sent successfully.';
+  } else {
+    echo 'Error sending email.';
+  }
+}
 ?>
